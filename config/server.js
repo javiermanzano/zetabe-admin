36adf7029d03b5a130495b32b7f57f0ed255fa5c
Javier Manzano <jmanzano@soamee.com>
2021-08-23 17:48:02 +0200

    database for postgres

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8ff76561f489b10d42ec569af30f333b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e4baa076-655f-4488-b5d2-ad898aec35f5'
+};
